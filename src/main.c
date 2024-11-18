8e6f964afe29f9e44e130ed5782c3251adaffb04
Kamaz-Birr <yohan.orewa>
2024-11-18 14:35:14 +0100

    second commit

--- src/main.c
+++ src/main.c
@@ -13,13 +13,13 @@ bool initialize_window(void) {
 	}
 	
 	// Create STL Window
-	window = STL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_BORDERLESS);
+	window = SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_BORDERLESS);
 	if (!window) {
 		fprintf(stderr, "Error creating SDL window.\n");
 		return false;
 	}
 	// Create an SDL Renderer
-	renderer = STL_CreateRenderer(window, -1, 0);
+	renderer = SDL_CreateRenderer(window, -1, 0);
 	if (!renderer) {
 		fprintf(stderr, "Error creating SDL renderer.\n");
 		return false;
@@ -33,7 +33,18 @@ void setup(void) {
 }
 
 void process_input(void) {
+	SDL_Event event;
+	SDL_PollEvent(&event);
 
+	switch(event.type) {
+		case SDL_QUIT:
+			is_running = false;
+			break;
+		case SDL_KEYDOWN:
+			if(event.key.keysym.sym == SDLK_ESCAPE)
+				is_running = false;
+			break;
+	}
 }
 
 void update(void) {
@@ -41,7 +52,10 @@ void update(void) {
 }
 
 void render(void) {
-	
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	SDL_RenderPresent(renderer);
 }
 
 int main() {
@@ -50,7 +64,7 @@ int main() {
 
 	setup();
 
-	while(true) {
+	while(is_running) {
 		process_input();
 		update();
 		render();
